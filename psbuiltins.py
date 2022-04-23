0d4202015bf2acec5693f483ac3239473db90dfc
Ben <ben.weberj@gmail.com>
2022-04-23 11:16:25 -0700

    Remove begin and end methods. Clean comments.

--- psbuiltins.py
+++ psbuiltins.py
@@ -31,18 +31,14 @@ class Stacks:
             "getinterval":self.getinterval,
             "putinterval":self.putinterval,
             "search" : self.search,
-            "begin":self.begin,
-            "end":self.end,
             "def":self.psDef,
             "if":self.psIf,
             "ifelse":self.psIfelse,
             "for":self.psFor
         }
-    #------- Operand Stack Helper Functions --------------
+
     
-    """
-        Helper function. Pops the top value from opstack and returns it.
-    """
+    # Pops the top value from opstack and returns it.
     def opPop(self):
         if len(self.opstack) > 0:
             x = self.opstack[len(self.opstack) - 1]
@@ -51,43 +47,34 @@ class Stacks:
         else:
             print("Error: opPop - Operand stack is empty")
 
-    """
-       Helper function. Pushes the given value to the opstack.
-    """
+
+    # Pushes $value to the opstack.
     def opPush(self,value):
         self.opstack.append(value)
 
-    #------- Dict Stack Helper Functions --------------
-    """
-       Helper function. Pops the top dictionary from dictstack and returns it.
-    """  
+
+    # Pops the top dictionary from dictstack and returns it.
     def dictPop(self):
         if len(self.dictstack) > 0:
             return self.dictstack.pop()
         else:
             print('Error, no dictionaries on dictstack')
 
-    """
-       Helper function. Pushes the given dictionary onto the dictstack. 
-    """   
+
+    # Pushes the given dictionary onto the dictstack. 
     def dictPush(self,d):
         self.dictstack.append(d)
 
-    """
-       Helper function. Adds name:value pair to the top dictionary in the dictstack.
-       (Note: If the dictstack is empty, first adds an empty dictionary to the dictstack then adds the name:value to that. 
-    """  
+
+    # Adds $name:$value pair to the top dictionary in the dictstack.
     def define(self, name, value):
         if len(self.dictstack) == 0:
            self.dictPush({ name: value })
         else:
             self.dictstack[-1][name] = value
 
-    """
-       Helper function. Searches the dictstack for a variable or function and returns its value. 
-       (Starts searching at the top of the dictstack; if name is not found returns None and prints an error message.
-        Make sure to add '/' to the begining of the name.)
-    """
+
+    # Searches the dictstack for a variable or function and returns its value.
     def lookup(self, name):
         name = '/' + name
         ds = self.dictstack[:]
@@ -95,15 +82,10 @@ class Stacks:
         for entry in ds:
             if name in entry:
                 return entry[name]
-
         return None
 
-    
-    #------- Arithmetic Operators --------------
 
-    """
-       Pops 2 values from opstack; checks if they are numerical (int); adds them; then pushes the result back to opstack. 
-    """  
+    # if $op1 and $op2 are numbers, add them and push the sum to the opstack.
     def add(self):
         if len(self.opstack) > 1:
             op1 = self.opPop()
@@ -117,9 +99,8 @@ class Stacks:
         else:
             print("Error: add expects 2 operands")
 
-    """
-       Pops 2 values from opstack; checks if they are numerical (int); subtracts them; and pushes the result back to opstack. 
-    """ 
+
+    # if $op1 and $op2 are numbers, subtract them and push the result to the opstack.
     def sub(self):
         if len(self.opstack) > 1:
             op1 = self.opPop()
@@ -133,9 +114,8 @@ class Stacks:
         else:
             print("Error: sub expects 2 operands")
 
-    """
-        Pops 2 values from opstack; checks if they are numerical (int); multiplies them; and pushes the result back to opstack. 
-    """
+
+    # if $op1 and $op2 are numbers, multiply them and push the product to the opstack.
     def mul(self):
         if len(self.opstack) > 1:
             op1 = self.opPop()
@@ -149,10 +129,8 @@ class Stacks:
         else:
             print("Error: sub expects 2 operands")
 
-    """
-        Pops 2 values from stack; checks if they are int values; calculates the remainder of dividing the bottom value by the top one; 
-        pushes the result back to opstack.
-    """
+
+    # if $op1 and $op2 are numbers, mod $op1 by $op2 and push the remainder to the stack.
     def mod(self):
         if len(self.opstack) > 1:
             op1 = self.opPop()
@@ -166,11 +144,8 @@ class Stacks:
         else:
             print("Error: sub expects 2 operands")
 
-    """ Pops 2 values from stacks; if they are equal pushes True back onto stack, otherwise it pushes False.
-          - if they are integers or booleans, compares their values. 
-          - if they are StrConstant values, compares the `value` attributes of the StrConstant objects;
-          - if they are DictConstant objects, compares the objects themselves (i.e., ids of the objects).
-        """
+
+    # if $op1 and $op2 are equal, push True to the opstack. Otherwise push False.
     def eq(self):
         if len(self.opstack) > 1:
             op1 = self.opPop()
@@ -185,16 +160,10 @@ class Stacks:
                 # self.opPush(op1.value is op2.value)
             else:
                 self.opPush(op1 == op2)
-
-
         else:
             print('Error: eq expects 2 argumnents')
 
-    """ Pops 2 values from stacks; if the bottom value is less than the second, pushes True back onto stack, otherwise it pushes False.
-          - if they are integers or booleans, compares their values. 
-          - if they are StrConstant values, compares the `value` attributes of them;
-          - if they are DictConstant objects, compares the objects themselves (i.e., ids of the objects).
-    """  
+    # if $op2 < $op1, push True to the opstack. Otherwise push False.
     def lt(self):
         if len(self.opstack) > 1:
             op1 = self.opPop()
@@ -211,11 +180,7 @@ class Stacks:
             print('Error: lt expects 2 arguments')
 
 
-    """ Pops 2 values from stacks; if the bottom value is greater than the second, pushes True back onto stack, otherwise it pushes False.
-          - if they are integers or booleans, compares their values. 
-          - if they are StrConstant values, compares the `value` attributes of them;
-          - if they are DictConstant objects, compares the objects themselves (i.e., ids of the objects).
-    """  
+    # if $op2 > $op1, push True to the opstack. Otherwise push False.
     def gt(self):
         if len(self.opstack) > 1:
             op1 = self.opPop()
@@ -231,19 +196,16 @@ class Stacks:
         else:
             print('Error: lt expects 2 arguments')
 
-    #------- Stack Manipulation and Print Operators --------------
-    """
-       This function implements the Postscript "pop operator". Calls self.opPop() to pop the top value from the opstack and discards the value. 
-    """
+
+    # Removes the top value from the opstack
     def pop (self):
         if (len(self.opstack) > 0):
             self.opPop()
         else:
             print("Error: pop expects 1 argument")
 
-    """
-       Prints the opstack and dictstack. The end of the list is the top of the stack. 
-    """
+
+    # Prints the opstack and dictstack. The end of the list is the top of the stack.
     def stack(self):
         print(OKGREEN+"**opstack**")
         for item in reversed(self.opstack):
@@ -255,18 +217,15 @@ class Stacks:
         print("-----------------------"+ CEND)
 
 
-    """
-       Copies the top element in opstack.
-    """
+    # Copies the top element in opstack and pushes it to the opstack.
     def dup(self):
         if len(self.opstack) > 0:
             self.opPush(self.opstack[-1])
         else:
             print('Error - dup expects 1 argument')
 
-    """
-       Pops an integer count from opstack, copies count number of values in the opstack. 
-    """
+
+    # Copies $count number of items in the opstack and pushes them to the top
     def copy(self):
         if len(self.opstack) < 1: print('Error: copy expects 1 argument')
         else:
@@ -277,21 +236,16 @@ class Stacks:
             while len(temp) != 0:
                 self.opPush(temp.pop())
 
-    """
-        Counts the number of elements in the opstack and pushes the count onto the top of the opstack.
-    """
+
+    # Counts the number of elements in the opstack and pushes the count to the opstack.
     def count(self):
         self.opPush(len(self.opstack))
 
-    """
-       Clears the opstack.
-    """
+    # Clears the opstack.
     def clear(self):
         self.opstack[:] = []
 
-    """
-       swaps the top two elements in opstack
-    """
+    # swaps the top two elements in opstack
     def exch(self):
         if len(self.opstack) < 2: print('Error: exch expects 2 arguments')
 
@@ -300,10 +254,8 @@ class Stacks:
         self.opPush(a1)
         self.opPush(a2)
 
-    # ------- String and Dictionary creator operators --------------
 
-    """ Creates a new empty string  pushes it on the opstack.
-    Initializes the characters in the new string to \0 , i.e., ascii NUL """
+    # pushes empty string (characters initialized to \0) to the opstack
     def string(self):
         space = 1
         if len(self.opstack) > 0 and isinstance(self.opstack[-1], int): space = int(self.opPop())
@@ -312,15 +264,13 @@ class Stacks:
         for i in range(space): blankStr += '\0'
         self.opPush(StrConstant(f'({blankStr})'))
     
-    """Creates a new empty dictionary  pushes it on the opstack """
+    # pushes an empty dictionary to the opstack
     def psDict(self):
         self.opPop()
         self.opPush(DictConstant({}))
 
-    # ------- String and Dictionary Operators --------------
-    """ Pops a string or dictionary value from the operand stack and calculates the length of it. Pushes the length back onto the stack.
-       The `length` method should support both DictConstant and StrConstant values.
-    """
+
+    # pushes the length of $item (string or dictionary) to the opstack
     def length(self):
         if len(self.opstack) < 1:
             print('Error: length expects 1 argument')
@@ -332,12 +282,10 @@ class Stacks:
                 self.opPush(len(item.value))
 
 
-    """ Pops either:
-         -  "A (zero-based) index and an StrConstant value" from opstack OR 
-         -  "A `name` (i.e., a key) and DictConstant value" from opstack.  
-        If the argument is a StrConstant, pushes the ascii value of the the character in the string at the index onto the opstack;
-        If the argument is an DictConstant, gets the value for the given `name` from DictConstant's dictionary value and pushes it onto the opstack
-    """
+    # if $item is a string:
+    #   - pushes the ascii value of the character at index $key in string $item to the opstack
+    # if $item is a dictionary:
+    #   - pushes the value of the key $key to the opstack
     def get(self):
         if len(self.opstack) < 2:
             print('Error: get expects 2 arguments')
@@ -351,14 +299,12 @@ class Stacks:
                 self.opPush(item.value[key])
             else:
                 print('Error: not sure what you\'re trying to get.')
-   
-    """
-    Pops either:
-    - "An `item`, a (zero-based) `index`, and an StrConstant value from  opstack", OR
-    - "An `item`, a `name`, and a DictConstant value from  opstack". 
-    If the argument is a StrConstant, replaces the character at `index` of the StrConstant's string with the character having the ASCII value of `item`.
-    If the argument is an DictConstant, adds (or updates) "name:item" in DictConstant's dictionary `value`.
-    """
+
+
+    # if $item is a string:
+    #   - replaces the character at index $key with ASCII $token in the string $item
+    # if $item is s dictionary:
+    #   - adds or updates the key $key in dictionary $item to be $token
     def put(self):
         if len(self.opstack) < 3:
             print('Error: put requires 3 arguments')
@@ -375,12 +321,9 @@ class Stacks:
             else:
                 # print(f'key: {key}, item: {item}, token: {token}')
                 print('Error: not sure what you\'re trying to put into what.')
-    """
-    getinterval is a string only operator, i.e., works only with StrConstant values. 
-    Pops a `count`, a (zero-based) `index`, and an StrConstant value from  opstack, and 
-    extracts a substring of length count from the `value` of StrConstant starting from `index`,
-    pushes the substring back to opstack as a StrConstant value. 
-    """ 
+    
+
+    # pushes to substring of $item from indices $ind to ($ind + $count) to the opstack
     def getinterval(self):
         if len(self.opstack) < 3:
             print('Error: getinterval expects 3 arguments')
@@ -395,12 +338,7 @@ class Stacks:
             self.opPush(StrConstant(f'({snip})'))
 
 
-
-    """
-    putinterval is a string only operator, i.e., works only with StrConstant values. 
-    Pops a StrConstant value, a (zero-based) `index`, a `substring` from  opstack, and 
-    replaces the slice in StrConstant's `value` from `index` to `index`+len(substring)  with the given `substring`s value. 
-    """
+    # replaces the contents $item at the indices $index to ($index + len($substr)) with $substr
     def putinterval(self):
         if len(self.opstack) < 3:
             print('Error: putinterval expects 3 parameters')
@@ -414,16 +352,11 @@ class Stacks:
 
             item.value = f'({i[:ind] + s + i[ind+len(s):]})'
 
-    """
-    search is a string only operator, i.e., works only with StrConstant values. 
-    Pops two StrConstant values: delimiter and inputstr
-    if delimiter is a sub-string of inputstr then, 
-       - splits inputstr at the first occurence of delimeter and pushes the splitted strings to opstack as StrConstant values;
-       - pushes True 
-    else,
-        - pushes  the original inputstr back to opstack
-        - pushes False
-    """
+
+    # if $delim is a substring of $st:
+    #   - split $st at first occurence of $delim and push both ends to the opstack
+    #   - push True to the opstack
+    # otherwise: push the originial string $st and False to the opstack
     def search(self):
         if len(self.opstack) < 2:
             print('Error: search expects 2 parameters')
@@ -440,24 +373,9 @@ class Stacks:
             else:
                 self.opPush(st)
                 self.opPush(False)
+    
 
-    # ------- Operators that manipulate the dictstact --------------
-    """ begin operator
-        Pops a DictConstant value from opstack and pushes it's `value` to the dictstack."""
-    def begin(self):
-        if (len(self.opstack) < 1):
-            print('Error: begin expects a single argument')
-        elif not isinstance(self.opstack[-1], DictConstant):
-            print('Error: no dictionary in stack')
-        else:
-            self.dictPush(self.opPop().value)
-
-    """ end operator
-        Pops the top dictionary from dictstack."""
-    def end(self):
-        self.dictPop()
-        
-    """ Pops a name and a value from stack, adds the definition to the dictionary at the top of the dictstack. """
+    # Defines $name to be $value in the current referencing environment (top of ditcstack)
     def psDef(self):
         if len(self.opstack) >= 2:
             value = self.opPop()
@@ -472,11 +390,7 @@ class Stacks:
             print('Error: def expected 2 arguments')
 
 
-    # ------- if/ifelse Operators --------------
-    """ if operator
-        Pops a Block and a boolean value, if the value is True, executes the code array by calling apply.
-       Will be completed in part-2. 
-    """
+    # if operator: if $bool_value is true, executes $block, otherwise nothing is done
     def psIf(self):
         if len(self.opstack) >= 2:
             block = self.opPop()
@@ -487,10 +401,8 @@ class Stacks:
         else:
             raise Exception('psIf expects 2 operands')
 
-    """ ifelse operator
-        Pops two Blocks and a boolean value, if the value is True, executes the bottom Block otherwise executes the top Block.
-        Will be completed in part-2. 
-    """
+
+    # ifelse operator: if $bool_value is true, executes $if_block, otherwise it executes $else_blkock
     def psIfelse(self):
         if len(self.opstack) >= 3:
             else_block = self.opPop()
@@ -505,14 +417,8 @@ class Stacks:
             raise Exception('psIfElse expects 3 operands')
 
 
-    #------- Loop Operators --------------
-    """
-       Implements for operator.   
-       Pops a Block, the end index (end), the increment (inc), and the begin index (begin) and 
-       executes the code array for all loop index values ranging from `begin` to `end`. 
-       Pushes the current loop index value to opstack before each execution of the Block. 
-       Will be completed in part-2. 
-    """ 
+    # for loop operator: executes $block for indices $begin through $end, incrementing by $step.
+    # Pushes current index $i to the opstack before each execution of $block
     def psFor(self):
         if len(self.opstack) >= 4:
             block = self.opPop()
@@ -534,12 +440,14 @@ class Stacks:
         else:
             raise Exception('psFor expects 4 operands')
 
-    """ Cleans both stacks. """      
+
+    # Clears both stacks.
     def clearBoth(self):
         self.opstack[:] = []
         self.dictstack[:] = []
 
-    """ Will be needed for part2"""
+
+    # Removes None value from top of opstack, if present
     def cleanTop(self):
         if len(self.opstack)>1:
             if self.opstack[-1] is None:
